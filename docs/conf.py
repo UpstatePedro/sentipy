fc0537088992998361d1a892d74a7f37c1dfd5cd
UpstatePedro <piers.m.holden@gmail.com>
2020-05-29 12:02:31 +0100

    readthedocs troubleshooting

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,10 @@ language = 'en'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- ReadTheDocs configuration ---------------------------------------------------
+
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
